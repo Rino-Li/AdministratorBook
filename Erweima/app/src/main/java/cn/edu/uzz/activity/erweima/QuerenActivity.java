1a1ba7e174c823dce82433a0fe45a82453a3f1d7
ITjunjun <1653672996@qq.com>
2018-01-28 12:02:42 +0800

    4
    改进了上传方法，还差界面没有优化，如果条件允许，可以加一些其他管理功能

--- Erweima/app/src/main/java/cn/edu/uzz/activity/erweima/QuerenActivity.java
+++ Erweima/app/src/main/java/cn/edu/uzz/activity/erweima/QuerenActivity.java
@@ -2,28 +2,31 @@ package cn.edu.uzz.activity.erweima;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class QuerenActivity extends AppCompatActivity implements View.OnClickListener{
-	private String[] typeid=new String[4];
-	private int lenth=0;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuerenActivity extends AppCompatActivity implements View.OnClickListener {
+	private String[] typeid = new String[4];
+	private int lenth = 0;
 	private String result;
 	private Button querenBtn;
-	private int type1;
-	private int id1;
-	private int type2;
-	private int id2;
+	private JSONObject data = new JSONObject();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,28 +35,37 @@ public class QuerenActivity extends AppCompatActivity implements View.OnClickLis
 	}
 
 	private void initView() {
-		result=getIntent().getStringExtra("book");
-		querenBtn=findViewById(R.id.queren);
+		result = getIntent().getStringExtra("book");
+		Log.e("BBBB", result);
+		querenBtn = findViewById(R.id.queren);
 		querenBtn.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.queren:
+				queren();
+				break;
+			default:
+				break;
+		}
+	}
+
 	private void queren() {
-		//1创建请求队列
-		RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-		//2创建请求
-		JsonObjectRequest request=new JsonObjectRequest(
-				"http://123.206.230.120/Book/rentoneServ?result="+result, null, new Response.Listener<JSONObject>() {
+		final RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+		StringRequest stringrequest=new StringRequest(Request.Method.POST, "http://192.168.218.2:8080/Book/rentoneServ", new Response.Listener<String>() {
 			@Override
-			public void onResponse(JSONObject response) {
+			public void onResponse(String s) {
 				try {
-					int resultCode = response.getInt("resultCode");
-					int errrorNum=response.getInt("errorNum");
-					if (resultCode == 3) {
-						Toast.makeText(QuerenActivity.this,"用户取消了借阅",Toast.LENGTH_SHORT).show();
-					} else if (resultCode == 4) {
+					JSONObject jsonObject=new JSONObject(s);
+					Log.e("BBBB",s);
+					int rentLength=jsonObject.getInt("rentLength");
+					int errorNum=jsonObject.getInt("errorNum");
+					if(errorNum==0){
 						Toast.makeText(QuerenActivity.this,"借阅成功",Toast.LENGTH_SHORT).show();
-					} else if (resultCode == 2||resultCode==1) {
-						Toast.makeText(QuerenActivity.this,"系统错误，请稍后再试",Toast.LENGTH_SHORT).show();
+					}else {
+						Toast.makeText(QuerenActivity.this,errorNum+" 本借阅失败",Toast.LENGTH_SHORT).show();
 					}
 				} catch (JSONException e) {
 					e.printStackTrace();
@@ -62,20 +74,18 @@ public class QuerenActivity extends AppCompatActivity implements View.OnClickLis
 		}, new Response.ErrorListener() {
 			@Override
 			public void onErrorResponse(VolleyError volleyError) {
-				Toast.makeText(QuerenActivity.this,"系统错误，请稍后再试",Toast.LENGTH_SHORT).show();
+				Log.e("VolleyError---", volleyError.getMessage(), volleyError);
+				byte[] htmlBodyBytes = volleyError.networkResponse.data;  //回应的报文的包体内容
+				Log.e("VolleyError body---->", new String(htmlBodyBytes), volleyError);
+				return;
 			}
-		});
-		//3请求加入队列
-		queue.add(request);
-	}
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()){
-			case R.id.queren:
-				queren();
-				break;
-			default:
-				break;
-		}
+		}){
+			protected Map<String,String> getParams(){
+				Map<String,String> map=new HashMap<String, String>();
+				map.put("result",result);
+				return map;
+			}
+		};
+		requestQueue.add(stringrequest);
 	}
 }
